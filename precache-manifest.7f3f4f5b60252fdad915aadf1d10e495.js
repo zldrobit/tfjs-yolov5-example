0a21572b1087a125aa3a12be2dcc8871966a8307
Jiacong Fang <zldrobit@126.com>
2021-07-05 11:22:38 +0800

    Updates

---
+++ precache-manifest.7f3f4f5b60252fdad915aadf1d10e495.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0580d789416dd2d97203",
+    "url": "/tfjs-yolov5-demo/static/css/main.70927434.chunk.css"
+  },
+  {
+    "revision": "0580d789416dd2d97203",
+    "url": "/tfjs-yolov5-demo/static/js/main.0580d789.chunk.js"
+  },
+  {
+    "revision": "2eb2a4e236a9ed2d2fa4",
+    "url": "/tfjs-yolov5-demo/static/js/1.2eb2a4e2.chunk.js"
+  },
+  {
+    "revision": "063e23ed4e35f45ba425",
+    "url": "/tfjs-yolov5-demo/static/js/runtime~main.063e23ed.js"
+  },
+  {
+    "revision": "b367dee0dd1a7b8b3ba99d22b526c6f3",
+    "url": "/tfjs-yolov5-demo/index.html"
+  }
+];
